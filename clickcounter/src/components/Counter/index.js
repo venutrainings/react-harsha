869c1fd0828244e7bf50214764c43c83c0d7b12f
harsha <harsha@rsaamerica.com>
2022-04-20 08:22:07 -0500

    first commit

---
+++ clickcounter/src/components/Counter/index.js
@@ -0,0 +1,53 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  }
+
+  onIncrement = () => {
+    this.setState(prevState => {
+      console.log(`Previous State Value ${prevState.count} `)
+      return {count: prevState.count + 1}
+    })
+  }
+
+  onDecrement = () => {
+    const {count} = this.state
+    if (count > 0) {
+      this.setState(prevState => {
+        console.log(`Previous State Value ${prevState.count} `)
+        return {count: prevState.count - 1}
+      })
+    }
+  }
+
+  onReset = () => {
+    this.setState({count: 0})
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">Counter</h1>
+        <p className="count"> {count}</p>
+        <div>
+          <button className="button" onClick={this.onIncrement}>
+            Increase
+          </button>
+          <button className="button" onClick={this.onReset}>
+            Reset
+          </button>
+          <button className="button" onClick={this.onDecrement}>
+            Decrease
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter
